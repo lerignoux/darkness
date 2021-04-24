9b69766111839ac56d791cbaa830644cb39ea589
Laurent Erignoux <lerignoux@gmail.com>
2021-04-24 15:21:18 +0800

    Adding frontend draft with echart support

---
+++ web/src/main.js
@@ -0,0 +1,30 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import ECharts from 'vue-echarts'
+import { use } from "echarts/core"
+
+// import ECharts modules manually to reduce bundle size
+import {
+  CanvasRenderer
+} from 'echarts/renderers'
+import {
+  BarChart
+} from 'echarts/charts'
+import {
+  GridComponent,
+  TooltipComponent
+} from 'echarts/components'
+
+use([
+  CanvasRenderer,
+  BarChart,
+  GridComponent,
+  TooltipComponent
+])
+
+const app = createApp(App).mount('#app')
+
+// register globally (or you can do it locally)
+app.component('v-chart', ECharts)
+
+app.mount()
